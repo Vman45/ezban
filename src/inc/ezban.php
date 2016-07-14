14165b3847e33a7d634f88115ff13ce5a6bdbd7b
Tyler Carney <tyler.carney@gmail.com>
2016-07-13 23:08:04 -0700

    screenshots of both lists with test data

--- src/inc/ezban.php
+++ src/inc/ezban.php
@@ -5,8 +5,8 @@ function ban($StartIP,$EndIP,$Description,$Expires,$days_to_ban,$InfoURL) {
         $mycon = condb();
         if($mycon) { # returns false if it failed
                 $stmt = $mycon->prepare('INSERT INTO banranges (StartIP,EndIP,Description,Expires,days_to_ban,InfoURL) VALUES (:StartIP,:EndIP,:Description,:Expires,:days_to_ban,:InfoURL);');
-                $stmt->bindParam(':StartIP',$StartIP);
-                $stmt->bindParam(':EndIP',$EndIP);
+                $stmt->bindParam(':StartIP',ip2long($StartIP));
+                $stmt->bindParam(':EndIP',ip2long($EndIP));
                 $stmt->bindParam(':Description',$Description);
                 $stmt->bindParam(':InfoURL',$InfoURL);
                 $stmt->bindParam(':Expires',$Expires);
@@ -24,7 +24,7 @@ function show_banlist() {
 		$stmt = $mycon->prepare('SELECT StartIP FROM banranges;');
 		$stmt->execute();
 		while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-			echo($result['StartIP']);
+			echo(long2ip($result['StartIP']));
 			print("\n");
 		}
 	} else { # oops, it failed to connect to the db for some reason...
@@ -39,7 +39,7 @@ function show_p2p_banlist() {
 		$stmt = $mycon->prepare('SELECT StartIP,EndIP,Description FROM banranges;');
 		$stmt->execute();
 		while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-			echo($result['Description'].':'.$result['StartIP'].'-'.$result['EndIP']);
+			echo($result['Description'].':'.long2ip($result['StartIP']).'-'.long2ip($result['EndIP']));
 			print("\n");
 		}
 	} else { # oops, it failed to connect to the db for some reason...
